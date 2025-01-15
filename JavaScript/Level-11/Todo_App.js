0de589fc0490eddba3de7366d1d38d6c73d26e9b
Mulla Abdul Khader Jeelani <jeelanmohammed529@gmail.com>
2025-01-15 19:20:22 +0530

    Simple todo app

---
+++ JavaScript/Level-11/Todo_App.js
@@ -0,0 +1,22 @@
+let btn = document.querySelector('button');
+let inp = document.querySelector('input');
+let ul = document.querySelector('ul');
+btn.addEventListener('click', function () {
+    let li = document.createElement('li');
+    li.innerText = inp.value;
+
+    let delbtn = document.createElement('button');
+    delbtn.innerText = 'delete';
+    delbtn.classList.add('delete');
+    li.appendChild(delbtn);
+    ul.appendChild(li);
+    inp.value = '';
+})
+
+ul.addEventListener("click", function (event) {
+    if (event.target.nodeName == "BUTTON") {
+    let delItem=event.target.parentElement;
+    delItem.remove();
+    console.log('deleted');
+    }
+});
